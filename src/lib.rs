93a302530f8927a991a9b13467bfadb4e88601c5
7Limes <100952188+7Limes@users.noreply.github.com>
2022-06-04 15:04:23 -0400

    update to 0.1.2
    changed &String to &str in function args

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-const COLORS: [char; 9] = ['k','r','g','y','b','m','c','w','0'];
+const COLORS_ABV: [char; 9] = ['k','r','g','y','b','m','c','w','0'];
 const COLOR_NAMES: [&str; 8] = ["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"];
 
 /// Convert shorthand color codes into actual color codes.
@@ -9,7 +9,7 @@ const COLOR_NAMES: [&str; 8] = ["black", "red", "green", "yellow", "blue", "mage
 /// - `$[char]` - Foreground color
 /// - `#[char]` - Background color
 ///
-/// `[char]` must be one of the characters found in the `COLORS` constant.
+/// `[char]` must be one of the characters found in the `COLORS_ABV` constant.
 /// Here is the character for each color:
 /// - `black` - `k`
 /// - `red` - `r`
@@ -27,10 +27,10 @@ const COLOR_NAMES: [&str; 8] = ["black", "red", "green", "yellow", "blue", "mage
 /// Example
 /// ```
 /// use terminalcolor as tc;
-/// let s = tc::convert(&String::from("$rHello, $Bworld!"));
+/// let s = tc::convert("$rHello, $Bworld!");
 /// println!("{}", s);
 /// ```
-pub fn convert(s: &String) -> String {
+pub fn convert(s: &str) -> String {
     let mut out = String::new();
     let mut i: usize = 0;
     while i < s.len() {
@@ -40,7 +40,7 @@ pub fn convert(s: &String) -> String {
             break;
         }
         let color_code = s.chars().nth(i+1).unwrap();
-        if (c != '$' && c != '#') || !COLORS.contains(&color_code.to_ascii_lowercase()) {
+        if (c != '$' && c != '#') || !COLORS_ABV.contains(&color_code.to_ascii_lowercase()) {
             out.push(c);
             i += 1;
             continue;
@@ -50,7 +50,7 @@ pub fn convert(s: &String) -> String {
             i += 2;
             continue;
         }
-        let color_index = COLORS.iter().position(|&x| x == color_code.to_ascii_lowercase()).unwrap();
+        let color_index = COLORS_ABV.iter().position(|&x| x == color_code.to_ascii_lowercase()).unwrap();
         let mut add = match c {
             '$' => 30,
             '#' => 40,
@@ -68,26 +68,35 @@ pub fn convert(s: &String) -> String {
 }
 
 /// Print string with color. Syntax specified in `convert()` documentation.
-pub fn printf(s: &String) {
+pub fn printf(s: &str) {
     print!("{}", convert(s));
 }
 
 /// Print string `s` with color with a newline. Syntax specified in `convert()` documentation.
-pub fn printlnf(s: &String) {
+pub fn printlnf(s: &str) {
     println!("{}", convert(s));
 }
 
 /// Print string `s` in color `color`.
 /// Supports ANSI colors.
-pub fn print(color: &str, s: &String) {
+pub fn print(color: &str, s: &str) {
     if COLOR_NAMES.contains(&color) {
         let index = COLOR_NAMES.iter().position(|&x| x == color).unwrap();
         print!("{}", format!("\x1b[{}m{}", index+30, s));
+    }
+    else {
+        print!("{}", s);
     }
 }
 
 /// Print string `s` in color `color` with a newline.
 /// Supports ANSI colors.
-pub fn println(color: &str, s: &String) {
-    print(color, &format!("{}\n", s));
+pub fn println(color: &str, s: &str) {
+    if COLOR_NAMES.contains(&color) {
+        let index = COLOR_NAMES.iter().position(|&x| x == color).unwrap();
+        println!("{}", format!("\x1b[{}m{}", index+30, s));
+    }
+    else {
+        println!("{}", s);
+    }
 }
